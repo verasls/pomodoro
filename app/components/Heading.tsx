f6cb730f61e0ce40e3f5bba0648427186e0f4946
Lucas <lucasdsveras@gmail.com>
2024-02-29 21:54:11 -0300

    Update font sizes

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -10,11 +10,11 @@ const Heading = styled.h1<HeadingProps>`
     `
   font-size: ${
     props.as === "h1"
-      ? "2.4rem"
+      ? "2rem"
       : props.as === "h2"
-      ? "1.6rem"
-      : props.as === "h3"
       ? "1.2rem"
+      : props.as === "h3"
+      ? "0.8rem"
       : ""
   };
   font-weight: 500;
